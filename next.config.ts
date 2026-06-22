9724964f984bedcba353c2fc2fba16a64d612ba2
unknown <3122055197@qq.com>
2026-06-22 17:44:17 +0800

    配置修改

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  // 禁用 COZE 遥测上报
+  env: {
+    COZE_INTEGRATION_BASE_URL: '',
+    COZE_WORKLOAD_IDENTITY_API_KEY: '',
+  },
 };
 
 export default nextConfig;
